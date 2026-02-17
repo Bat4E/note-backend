df721d62206f937a474357a5b88abdafd9f3faf5
Bat4E <145248199+Bat4E@users.noreply.github.com>
2026-02-17 14:11:31 -0800

    FSO part 4 structuring project into best practices

--- models/note.js
+++ models/note.js
@@ -3,23 +3,11 @@
 // variables defined here such as: mongoose and url are not visible or accessible to users
 // of the module
 
+/*
+Will only define the Mongoose schema for notes
+*/
+
 const mongoose = require("mongoose");
-
-mongoose.set("strictQuery", false);
-
-// database connection URL is passed to the app via the MONGODB_URI env variable
-// as hardcoding it is not a good idea!
-const url = process.env.MONGODB_URI;
-
-console.log("connecting to", url);
-mongoose
-  .connect(url) // this one just has only param url
-  .then((result) => {
-    console.log("connected to MongoDB");
-  })
-  .catch((error) => {
-    console.log("error connecting to MongoDB:", error.message);
-  });
 
 // Mongoose has its own validation
 const noteSchema = new mongoose.Schema({
